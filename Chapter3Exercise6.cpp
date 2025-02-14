a2c7d6f072b1d2c530464ef3328b910457236163
Charles Sherwood <kulan876321@yahoo.com>
2025-02-14 15:04:59 -0500

    Finished

--- Chapter3Exercise6.cpp
+++ Chapter3Exercise6.cpp
@@ -14,21 +14,35 @@ using namespace std;
 int main()
 {
 
+	//Create variables 
 
 	double OGsugar = 1.5;
 	double OGbutter = 1;
 	double OGflour = 2.75;
 	double OGcookies = 48;
+	double cookies;
+	double Newcookie;
 
+	//Get the amount of cookies the user wants.
 	cout << "Please enter the amount of cookies that you would like to make:";
-	cin
+	cin >> cookies;
+	cout << "You want to make " << cookies << endl;
 
+	//Divide the new cookies with the old amount.
+	Newcookie = cookies / OGcookies;
 
-	
-	
-	
-	
-	
+	//Then multiply all the measurements for the sugar.
+	double Sugar = OGsugar * Newcookie;
+	double Butter = OGbutter * Newcookie;
+	double Flour = OGflour * Newcookie;
+		
+
+	//Display all the information onto the screen.
+	cout << fixed<<setprecision(2)<<"Sugar:" << Sugar << " Cups of sugar" << endl;
+	cout <<fixed<<setprecision(2)<< "Butter:" << Butter << " Cups of butter" << endl;
+	cout << fixed<<setprecision(2)<<"Flour:" << Flour << " Cups of flour" << endl;
+	return 0;
+
 	
 	
 	
