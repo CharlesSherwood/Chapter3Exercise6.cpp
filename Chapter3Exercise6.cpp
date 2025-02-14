63d97ca4d0468a92b3585908a5edaae66e89a7e3
Charles Sherwood <kulan876321@yahoo.com>
2025-02-14 11:55:56 -0500

    Add project files.

---
+++ Chapter3Exercise6.cpp
@@ -0,0 +1,42 @@
+/*FilName:Chapter3Exercise6.cpp
+Programmer:Charles Sherwood
+Date:2/2025
+Requirements:Adjust the ingredients for a recipe that
+makes 48 cookies and the measurements are 1.5 cups of 
+sugar, 1 cup of butter, and 2.75 cups of flour
+*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+
+
+	double OGsugar = 1.5;
+	double OGbutter = 1;
+	double OGflour = 2.75;
+	double OGcookies = 48;
+
+	cout << "Please enter the amount of cookies that you would like to make:";
+	cin
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
